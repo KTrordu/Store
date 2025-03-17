0a36ba77ea45928b2405778b5d7f164042878519
KTrordu <alptug.yilmaz@outlook.com>
2025-03-17 09:48:46 +0300

    updated the structure

---
+++ StoreApp.UI/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StoreApp.DAL.ApplicationDbContext;
+
+namespace StoreApp.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly AppDbContext _db;
+
+        public ProductController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var model = _db.Products.ToList();
+
+            return await Task.FromResult((IActionResult) View(model));
+        }
+    }
+}
